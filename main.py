c55f4ee3ddf9b9a9571ace0065040fe928838475
SamipSGz <samipgh444@gmail.com>
2024-10-30 22:47:12 +0545

    localhost

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ model = genai.GenerativeModel('gemini-pro')
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5174", "http://localhost:3000","http://localhost:5174"],
+    allow_origins=["http://localhost:5174", "http://localhost:3000","http://localhost:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
